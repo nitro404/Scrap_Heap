f1edf76c7198ac4ad3ab89420b65b981a67a2963
Corey Ling <coreyling@ionshard.com>
2010-10-28 20:44:30 +0000

    Added Simple Skeleton Files
    Squashed commit of the following:
    
    commit 21854ac977e8395a552fbe88fd9013a27a6a857b
    Author: Corey Ling <coreyling@ionshard.com>
    Date:   Thu Oct 28 16:41:26 2010 -0400
    
        Skeleton Enemy.cs
    
    commit 0b592e45752211d6ae194228920366a43c308938
    Author: Corey Ling <coreyling@ionshard.com>
    Date:   Thu Oct 28 15:24:18 2010 -0400
    
        Skeleton Player.cs
    
    commit cffbf1b2e53d18290da24630789e44d5efc9cdf3
    Author: Corey Ling <coreyling@ionshard.com>
    Date:   Thu Oct 28 16:14:31 2010 -0400
    
        Skeleton Entity.cs
    
    git-svn-id: file:///e/SVN/sproket_engine@17 572b2360-8d81-40fd-bff0-8de276c2b01d

---
+++ Sproket Engine/Source/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SproketEngine.Source
+{
+    class Entity
+    {
+        private Vector3 m_position;
+        private Vector3 m_rotation;
+        private Model m_model;
+    }
+}
---
+++ Sproket Engine/Source/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SproketEngine.Source
+{
+   /*ID
+    * Name
+    * Position
+    * Rotation
+    * Health
+    * Model
+    * WeaponCollection
+          o All of the player's weapons
+    * AmmunitionCollection
+          o All of the player's ammo (handles shared aummunition types)
+    * Armor? Powerups
+    */
+    class Player : Entity
+    {
+        private int m_ID;
+        private string m_name;
+        private int m_health;
+        //private WeaponCollection m_weapons;
+        //private AmmunitionCollection m_ammo;
+
+    }
+}
